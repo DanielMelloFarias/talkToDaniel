12e3f5ca504d28803c25f5d70ea25fb263cec6ed
DanielMelloFarias <danielmellofarias@gmail.com>
2024-03-26 00:18:08 -0300

    update main.py

--- main.py
+++ main.py
@@ -102,23 +102,47 @@ avatar_url =  "https://iili.io/JjFrGXR.jpg"
 
 #################################################
 
+if "history" not in st.session_state:
+    st.session_state.history = []
+
 genai.configure(api_key = api_key)
 model = genai.GenerativeModel("gemini-pro")
+chat = model.start_chat(history = st.session_state.history)
 
-#chat = model.start_chat(history = st.session_state.history)
+with st.sidebar:
+    if st.button("Clear Chat Window", use_container_width=True, type="primary"):
+        st.session_state.history = []
+        st.rerun()
 
+for message in chat.history:
+    role ="assistant" if message.role == 'model' else message.role
+    with st.chat_message(role):
+        st.markdown(message.parts[0].text)
 
-chat = model.start_chat()
-
-def LLM_Response(question):
-    response = chat.send_message(question,stream=True)
-    return response
-
-user_quest = st.text_input("Ask a question:")
-btn = st.button("Ask")
-
-if btn and user_quest:
-    result = LLM_Response(user_quest)
-    st.subheader("Response : ")
-    for word in result:
-        st.text(word.text)
+if "app_key" in st.session_state:
+    if prompt := st.chat_input(""):
+        prompt = prompt.replace('\n', ' \n')
+        with st.chat_message("user"):
+            st.markdown(prompt)
+        with st.chat_message("assistant"):
+            message_placeholder = st.empty()
+            message_placeholder.markdown("Thinking...")
+            try:
+                full_response = ""
+                for chunk in chat.send_message(prompt, stream=True):
+                    word_count = 0
+                    random_int = random.randint(5,10)
+                    for word in chunk.text:
+                        full_response+=word
+                        word_count+=1
+                        if word_count == random_int:
+                            time.sleep(0.05)
+                            message_placeholder.markdown(full_response + "_")
+                            word_count = 0
+                            random_int = random.randint(5,10)
+                message_placeholder.markdown(full_response)
+            except genai.types.generation_types.BlockedPromptException as e:
+                st.exception(e)
+            except Exception as e:
+                st.exception(e)
+            st.session_state.history = chat.history
