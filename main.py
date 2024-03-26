b1b76a6927f37a6e32b557f400804c2d12937310
DanielMelloFarias <danielmellofarias@gmail.com>
2024-03-26 00:32:51 -0300

    update main.py

--- main.py
+++ main.py
@@ -122,14 +122,10 @@ if prompt := st.chat_input("O que você deseja perguntar ? Digite aqui sua dúvi
         message_placeholder = st.empty()
         full_response = ""
         assistant_response = model.generate_content(prompt)
+        print (assistant_response)
+        
+        message_placeholder.markdown(full_response + "▌")
+        message_placeholder.markdown(full_response)
 
-        # Simulate stream of response with milliseconds delay
-        for chunk in assistant_response.split():
-            full_response += chunk + " "
-            time.sleep(0.05)
-            # Add a blinking cursor to simulate typing
-            message_placeholder.markdown(full_response + "▌")
-            message_placeholder.markdown(full_response)
-
-            # Add assistant response to chat history
-            st.session_state.messages.append({"role": "assistant", "content": full_response})            
+        # Add assistant response to chat history
+        st.session_state.messages.append({"role": "assistant", "content": full_response})            
