6c10b2c45126a544aae6086b157f9c938066e5f2
DanielMelloFarias <danielmellofarias@gmail.com>
2024-03-27 12:11:15 -0300

    update teste.py - add main

--- pages/teste.py
+++ pages/teste.py
@@ -1,17 +1,22 @@
 import streamlit as st
 import pandas as pd
 
-# Título da página/aplicativo
-st.title("Visualizador de CSV com Streamlit")
+def main():
+    # Título da página/aplicativo
+    st.title("Visualizador de CSV com Streamlit")
 
-# Carregamento do arquivo
-uploaded_file = st.file_uploader("Carregue seu arquivo CSV aqui", type=["csv"])
+    # Carregamento do arquivo
+    uploaded_file = st.file_uploader("Carregue seu arquivo CSV aqui", type=["csv"])
 
-# Verifica se um arquivo foi carregado
-if uploaded_file is not None:
-    # Lendo o arquivo CSV
-    df = pd.read_csv(uploaded_file)
+    # Verifica se um arquivo foi carregado
+    if uploaded_file is not None:
+        # Lendo o arquivo CSV
+        df = pd.read_csv(uploaded_file)
 
-    # Exibindo o dataframe
-    st.write("Visualizando os dados:")
-    st.dataframe(df)
+        # Exibindo o dataframe
+        st.write("Visualizando os dados:")
+        st.dataframe(df)
+
+# Executa a função principal
+if __name__ == "__main__":
+    main()
