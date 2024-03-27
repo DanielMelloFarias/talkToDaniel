1f1be7927d6e452d4fa30c3be1f115299ddad666
DanielMelloFarias <danielmellofarias@gmail.com>
2024-03-27 12:13:05 -0300

    update teste.py - mudei a formatacao da leitura

--- pages/teste.py
+++ pages/teste.py
@@ -1,22 +1,23 @@
 import streamlit as st
 import pandas as pd
 
-def main():
-    # Título da página/aplicativo
-    st.title("Visualizador de CSV com Streamlit")
+# Título da página/aplicativo
+st.title("Visualizador de CSV com Streamlit")
 
-    # Carregamento do arquivo
-    uploaded_file = st.file_uploader("Carregue seu arquivo CSV aqui", type=["csv"])
+# Carregamento do arquivo
+uploaded_file = st.file_uploader("Carregue seu arquivo CSV aqui", type=["csv"])
 
-    # Verifica se um arquivo foi carregado
-    if uploaded_file is not None:
+# Verifica se um arquivo foi carregado
+if uploaded_file is not None:
         # Lendo o arquivo CSV
-        df = pd.read_csv(uploaded_file)
+    try:
+        df = pd.read_csv(uploaded_file, encoding='utf-8')
+    except UnicodeDecodeError:
+        try:
+            df = pd.read_csv(uploaded_file, encoding='ISO-8859-1')
+        except UnicodeDecodeError:
+            st.error("Não foi possível ler o arquivo com as codificações comuns. Por favor, verifique a codificação do arquivo e tente novamente.")
 
-        # Exibindo o dataframe
-        st.write("Visualizando os dados:")
-        st.dataframe(df)
-
-# Executa a função principal
-if __name__ == "__main__":
-    main()
+    # Exibindo o dataframe
+    st.write("Visualizando os dados:")
+    st.dataframe(df)
