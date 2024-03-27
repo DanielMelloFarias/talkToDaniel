3805c2b3b12c183da596b94e5e8d8440f6145f0e
DanielMelloFarias <danielmellofarias@gmail.com>
2024-03-27 12:10:12 -0300

    create teste.py

---
+++ pages/teste.py
@@ -0,0 +1,17 @@
+import streamlit as st
+import pandas as pd
+
+# Título da página/aplicativo
+st.title("Visualizador de CSV com Streamlit")
+
+# Carregamento do arquivo
+uploaded_file = st.file_uploader("Carregue seu arquivo CSV aqui", type=["csv"])
+
+# Verifica se um arquivo foi carregado
+if uploaded_file is not None:
+    # Lendo o arquivo CSV
+    df = pd.read_csv(uploaded_file)
+
+    # Exibindo o dataframe
+    st.write("Visualizando os dados:")
+    st.dataframe(df)
